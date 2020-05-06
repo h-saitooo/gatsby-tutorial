6ed6d5a54a6e564d0e85950bd73f0ee1900d87d2
Hiroki Saito <hiroki.saito@bit2byte.jp>
2020-05-07 00:00:58 +0900

    part 8

---
+++ gatsby-node.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  // graphql関数は Promise を返す
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/blog-post.js`),
+      context: {
+        // コンテキストに渡したデータを
+        // GraphQL変数としてページクエリで使用できる
+        slug: node.fields.slug,
+      }
+    })
+  })
+}
+
